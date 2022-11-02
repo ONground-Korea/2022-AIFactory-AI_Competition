a46506c07bca529cc42b3944c9dff12c08450978
ONground <78339280+ONground-Korea@users.noreply.github.com>
2022-11-02 13:58:04 +0900

    Add files via upload

---
+++ 4. NDLinear/NDLinear/model.py
@@ -0,0 +1,154 @@
+import torch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torch.nn import Transformer
+import random
+import torch.backends.cudnn as cudnn
+import numpy as np
+import math
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+torch.cuda.manual_seed_all(42)
+np.random.seed(42)
+cudnn.benchmark = False
+cudnn.deterministic = True
+random.seed(42)
+
+
+class moving_avg(nn.Module):
+    """
+    Moving average block to highlight the trend of time series
+    """
+
+    def __init__(self, kernel_size, stride):
+        super(moving_avg, self).__init__()
+        self.kernel_size = kernel_size
+        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+
+    def forward(self, x):
+        # padding on the both ends of time series
+        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        x = torch.cat([front, x, end], dim=1)
+        x = self.avg(x.permute(0, 2, 1))
+        x = x.permute(0, 2, 1)
+        return x
+
+
+class series_decomp(nn.Module):
+    """
+    Series decomposition block
+    """
+
+    def __init__(self, kernel_size):
+        super(series_decomp, self).__init__()
+        self.moving_avg = moving_avg(kernel_size, stride=1)
+
+    def forward(self, x):
+        moving_mean = self.moving_avg(x)
+        res = x - moving_mean
+        return res, moving_mean
+
+
+class NDLinear(nn.Module):
+    """
+    Decomposition-Linear
+    """
+
+    def __init__(self):
+        super(NDLinear, self).__init__()
+        self.seq_len = 14
+        self.pred_len = 28
+
+        # Decompsition Kernel Size
+        kernel_size = 25
+        self.decompsition = series_decomp(kernel_size)
+        self.individual = True
+        self.channels = 4
+
+        self.NLinear1 = nn.Linear(self.seq_len, 256)
+        self.NLinear2 = nn.Linear(256, self.seq_len)
+
+        self.Linear1 = nn.Linear(self.seq_len, 256)
+        self.Linear2 = nn.Linear(256, self.seq_len)
+        self.Linear3 = nn.Linear(self.seq_len, 256)
+        self.Linear4 = nn.Linear(256, self.seq_len)
+        self.Linear5 = nn.Linear(self.pred_len, 256)
+        self.Linear6 = nn.Linear(256, self.pred_len)
+
+        self.Linear = nn.Linear(self.channels, 1)
+        self.NonLinear = nn.Mish()
+
+        if self.individual:
+            self.Linear_Seasonal = nn.ModuleList()
+            self.Linear_Trend = nn.ModuleList()
+
+            for i in range(self.channels):
+                self.Linear_Seasonal.append(nn.Linear(self.seq_len, self.pred_len))
+                self.Linear_Trend.append(nn.Linear(self.seq_len, self.pred_len))
+
+                # Use this two lines if you want to visualize the weights
+                # self.Linear_Seasonal[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+                # self.Linear_Trend[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+        else:
+            self.Linear_Seasonal = nn.Linear(self.seq_len, self.pred_len)
+            self.Linear_Trend = nn.Linear(self.seq_len, self.pred_len)
+
+            # Use this two lines if you want to visualize the weights
+            # self.Linear_Seasonal.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+            # self.Linear_Trend.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+
+    def forward(self, x):
+        # x: [Batch, Input length, Channel]
+
+        ###### NLinear ######
+        seq_last = x[:, -1:, :].detach()
+
+        # block 1
+        x = x - seq_last
+        x = self.Linear1(x.permute(0, 2, 1)).permute(0, 2, 1)
+        x = self.NonLinear(x)
+        x = self.Linear4(x.permute(0, 2, 1)).permute(0, 2, 1)
+        x = x + seq_last
+        #####################
+
+        seasonal_init, trend_init = self.decompsition(x)
+        seasonal_init, trend_init = seasonal_init.permute(0, 2, 1), trend_init.permute(0, 2, 1)
+        seasonal_init = self.Linear2(self.NonLinear(self.Linear1(seasonal_init)))
+        trend_init = self.Linear4(self.NonLinear(self.Linear3(trend_init)))
+
+        if self.individual:
+            seasonal_output = torch.zeros([seasonal_init.size(0), seasonal_init.size(1), self.pred_len],
+                                          dtype=seasonal_init.dtype).to(seasonal_init.device)
+            trend_output = torch.zeros([trend_init.size(0), trend_init.size(1), self.pred_len],
+                                       dtype=trend_init.dtype).to(trend_init.device)
+            for i in range(self.channels):
+                seasonal_output[:, i, :] = self.Linear_Seasonal[i](seasonal_init[:, i, :])
+                trend_output[:, i, :] = self.Linear_Trend[i](trend_init[:, i, :])
+        else:
+
+            seasonal_output = self.Linear_Seasonal(seasonal_init)
+            trend_output = self.Linear_Trend(trend_init)
+
+        x = seasonal_output + trend_output
+        x = self.Linear6(self.NonLinear(self.Linear5(x)))
+        x = self.Linear(x.permute(0, 2, 1))
+        return x  # to [Batch, Output length, Channel]
+
+
+def Nong_metric(output, target):
+    nong_output = output[:, :14]
+    sajucha_mean_output = torch.mean(output[:, 21:], dim=1).reshape(-1, 1)
+    for i in range(7): nong_output = torch.cat([nong_output, sajucha_mean_output], dim=1)
+
+    target_output = target[:, :, :14]
+    shape = output.shape[0]
+    sajucha_mean_target = torch.mean(target[:, :, 21:], dim=2).reshape(shape, shape, 1)
+    for i in range(7): target_output = torch.cat([target_output, sajucha_mean_target], dim=2)
+
+    loss = nn.L1Loss()
+    return loss(nong_output, target_output)
---
+++ 4. NDLinear/NDLinear/submit.py
@@ -0,0 +1,137 @@
+import os
+import pandas as pd
+import numpy as np
+
+from tqdm.auto import tqdm
+
+from torch.utils.data import DataLoader
+
+from glob import glob
+
+import torch
+import random
+import torch.backends.cudnn as cudnn
+
+from AIFactory.NDLinear.data import testDataset
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+torch.cuda.manual_seed_all(42)
+np.random.seed(42)
+cudnn.benchmark = False
+cudnn.deterministic = True
+random.seed(42)
+
+pat = '15'
+
+for i in tqdm(range(10)):
+    data_list = glob(f'C:/Users/hjs/PycharmProjects/pytorch/AIFactory/preprocess2/test/set_{i}/*.csv')
+
+    # model test
+    if os.path.exists('./model_output') == False:
+        os.mkdir('./model_output')
+
+    if os.path.exists(f'./model_output/set_{i}') == False:
+        os.mkdir(f'./model_output/set_{i}')
+
+    for idx, data in enumerate(sorted(data_list)):
+        file_number = data.split('test_')[1].split('.')[0]
+        # print(file_number)
+        if i == 9 and file_number == '7':
+            save_df = pd.DataFrame([0 for i in range(28)])
+            save_df.to_csv(f'./model_output/set_{i}/predict_{file_number}.csv', index=False)
+            continue
+
+        test_dataset = testDataset(data)
+        test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+        try:
+            model = torch.load(f'models/model_{file_number}.pth')
+        except:
+            print('no model!!')
+
+        model.eval()
+        for (inputs, outputs) in test_loader:
+            inputs = inputs.to('cuda')
+            pred = model(inputs.float())
+            pred = pred.to('cpu').detach().squeeze(dim=2).numpy()
+
+        # 결과 저장
+        save_df = pd.DataFrame(pred).T
+        save_df.to_csv(f'./model_output/set_{i}/predict_{file_number}.csv', index=False)
+
+for k in tqdm(range(10)):
+    globals()[f'set_df_{k}'] = pd.DataFrame()
+    answer_df_list = glob(f'./model_output/set_{k}/*.csv')  # 예측한 결과 불러오기
+    pum_list = glob(
+        f'C:/Users/hjs/OneDrive - 고려대학교/고려대학교/3-2/농산물/aT_data/aT_test_raw/sep_{k}/*.csv')  # 기존 test input 불러오기
+    pummok = [a for a in pum_list if 'pummok' in a.split('/')[-1]]
+
+    for i in answer_df_list:
+        df = pd.read_csv(i)
+        number = i.split('_')[-1].split('.')[0]
+
+        base_number = 0
+        for p in pummok:
+            if number == p.split('_')[-1].split('.')[0]:
+                pum_df = pd.read_csv(p)
+
+                if len(pum_df) != 0:
+                    base_number = pum_df.iloc[len(pum_df) - 1][
+                        '해당일자_전체평균가격(원)']  # 기존 각 sep 마다 test input의 마지막 target 값 가져오기 (변동률 계산을 위해)
+                else:
+                    base_number = np.nan
+
+        globals()[f'set_df_{k}'][f'품목{number}'] = [base_number] + list(
+            df[df.columns[-1]].values)  # 각 품목당 순서를 t, t+1 ... t+28 로 변경
+
+    globals()[f'set_df_{k}'] = globals()[f'set_df_{k}'][[f'품목{col}' for col in range(37)]]  # 열 순서를 품목0 ~ 품목36 으로 변경
+
+date = [f'd+{i}' for i in range(1, 15)] + ['d+22 ~ 28 평균']
+
+for k in range(10):
+    globals()[f'answer_df_{k}'] = pd.DataFrame()
+    for c in globals()[f'set_df_{k}'].columns:
+        base_d = globals()[f'set_df_{k}'][c][0]  # 변동률 기준 t 값
+
+        ans_1_14 = []
+        for i in range(14):
+            ans_1_14.append(
+                (globals()[f'set_df_{k}'][c].iloc[i + 1] - base_d) / base_d)  # t+1 ~ t+14 까지는 (t+n - t)/t 로 계산
+
+        ans_22_28 = (globals()[f'set_df_{k}'][c][
+                     22:29].mean() - base_d) / base_d  # t+22 ~ t+28은 np.mean(t+22 ~ t+28) - t / t
+
+        globals()[f'answer_df_{k}'][f'{c} 변동률'] = ans_1_14 + [ans_22_28]
+
+    globals()[f'answer_df_{k}']['Set'] = k  # set 번호 설정
+    globals()[f'answer_df_{k}']['일자'] = date  # 일자 설정
+
+# 위에서 계산된 변동률 들을 합쳐주는 과정
+
+all_df = pd.DataFrame()
+for i in range(10):
+    if i == 0:
+        all_df = pd.concat([all_df, globals()[f'answer_df_{i}']], axis=1)
+    else:
+        all_df = pd.concat([all_df, globals()[f'answer_df_{i}']])
+
+all_df = all_df[['Set', '일자'] + list(all_df.columns[:-2])]
+all_df.reset_index(drop=True, inplace=True)
+
+# set, 일자 기억하기위해 따로 저장
+
+re_set = list(all_df['Set'])
+re_date = list(all_df['일자'])
+
+# 정답 양식 불러오기
+out_ans = pd.read_csv('answer_example.csv')
+
+# 두 dataframe 합치기 (nan + 숫자 = nan 이용)
+submit_df = all_df + out_ans
+
+submit_df['Set'] = re_set
+submit_df['일자'] = re_date
+
+# 최종 저장
+submit_df.to_csv(f'submit_{pat}.csv', index=False, encoding='utf-8-sig')
